da01fb1ce1b414fc21a0f7ca94c587894fddde0c
Chloechew <105104684+chloechewx@users.noreply.github.com>
2025-02-14 12:11:19 +0800

    Update GameScreen.java

--- lwjgl3/src/main/java/com/mygdx/game/lwjgl3/GameScreen.java
+++ lwjgl3/src/main/java/com/mygdx/game/lwjgl3/GameScreen.java
@@ -100,8 +100,7 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         if (!isPaused) {
-            minion.update(delta); // Update the minion's state
-        }
+            minion.update(delta);}
         map.render(camera);
         debugRenderer.renderDebug(camera, map, minion);
         batch.setProjectionMatrix(camera.combined);
