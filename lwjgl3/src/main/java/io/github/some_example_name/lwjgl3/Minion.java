55633ee5441c161b8705b6eca4d9554e2ed58b68
Uneasierdrake <Shawnlim11.12@gmail.com>
2025-02-17 01:53:19 +0800

    Updated Minion Movement
    Implemented and Fixed Minion Movement (To fix Minion Collision Detection)

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Minion.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Minion.java
@@ -35,19 +35,19 @@ public class Minion {
     private HPBar hpBar;
     private OrthographicCamera camera;
     
-    private int MPhase1 = generateRandomNumber(200, 270);
-    private int MPhase2 = generateRandomNumber(520, 570);
-    private int MPhase3 = generateRandomNumber(390, 460);
-    private int MPhase4 = generateRandomNumber(300, 390);
-    private int MPhase5 = generateRandomNumber(160, 250);
-    private int MPhase6 = 240;
+    private int MPhase1 = 240;	//Minimum 200 to 270,
+    private int MPhase2 = 560;	//Minimum 520 to 570,
+    private int MPhase3 = 400;	//Minimum 390 to 460,
+    private int MPhase4 = 300;	//Minimum 300 to 390,
+    private int MPhase5 = 220;	//Minimum 160 to 250,
+    private int MPhase6 = 250;	//Minimum 240,
     
-    private int stepsMoveP1 = 0;
-    private int stepsMoveP2 = 0;
-    private int stepsMoveP3 = 0;
-    private int stepsMoveP4 = 0;
-    private int stepsMoveP5 = 0;
-    private int stepsMoveP6 = 0;
+    private int stepsMoveP1 = 0;	//Track how many steps Minion move Right,
+    private int stepsMoveP2 = 0;	//Track how many steps Minion move Down,
+    private int stepsMoveP3 = 0;	//Track how many steps Minion move Right,
+    private int stepsMoveP4 = 0;	//Track how many steps Minion move Up,
+    private int stepsMoveP5 = 0;	//Track how many steps Minion move Right,
+    private int stepsMoveP6 = 0;	//Track how many steps Minion move Up,
     
     private boolean moveP2 = false;
     private boolean moveP3 = false;
@@ -66,6 +66,7 @@ public class Minion {
           this.hpBar = new HPBar(startX, startY + DEFAULT_HEIGHT + 5, DEFAULT_WIDTH, 8, maxHP, camera); // Position HP bar above minion
           this.camera = camera;
       }
+      
       //this is to animate the character movement
       private Animation<TextureRegion> createAnimation(float frameDuration) {
           TextureRegion[][] frames = TextureRegion.split(
@@ -84,26 +85,68 @@ public class Minion {
           return new Animation<>(frameDuration, runningFrames);
       }
       
-      private int generateRandomNumber(int min, int max) {
-          return (int) (Math.random() * (max - min + 1)) + min;
-      }
-      
       public void update(float delta) {
 	      	hpBar.setPosition(position.x, position.y + DEFAULT_HEIGHT + 5);
 	        stateTime += delta;
 	        float speed = DEFAULT_SPEED * delta;
-	        Vector2 originalPosition = new Vector2(position);
+	        //Vector2 originalPosition = new Vector2(position);
 	        
-	        //collision checking
-	        bounds.setPosition(position.x, position.y);
-	        if (map.isColliding(bounds)) {
-	            position.set(originalPosition);
+	        float permittedX = position.x;
+	        float permittedY = position.y;
+	        
+	        //Phase 1: Moving Right,
+	        if (!moveP2 && stepsMoveP1 < MPhase1) {
+	            position.x += speed;
+	            stepsMoveP1++;
 	        }
 	        
-	        ScreenBounds(); //character wont walk out of frame
+	        //Phase 2: Moving Down,
+	        if (stepsMoveP1 >= MPhase1 && stepsMoveP2 < MPhase2) {
+	        	moveP2 = true;
+	            position.y -= speed;
+	            stepsMoveP2++;
+	        }
+	        
+	        //Phase 3: Moving Right,
+	        if (stepsMoveP2 >= MPhase2 && stepsMoveP3 < MPhase3) {
+	        	moveP3 = true;
+	            position.x += speed;
+	            stepsMoveP3++;
+	        }
+	        
+	        //Phase 4: Moving Up,
+	        if (stepsMoveP3 >= MPhase3 && stepsMoveP4 < MPhase4) {
+	        	moveP4 = true;
+	            position.y += speed;
+	            stepsMoveP4++;
+	        }
+	        
+	        //Phase 5: Moving Right,
+	        if (stepsMoveP4 >= MPhase4 && stepsMoveP5 < MPhase5) {
+	        	moveP5 = true;
+	            position.x += speed;
+	            stepsMoveP5++;
+	        }
+	        
+	        //Phase 6: Moving Up,
+	        if (stepsMoveP5 >= MPhase5 && stepsMoveP6 < MPhase6) {
+	        	moveP6 = true;
+	            position.y += speed;
+	            stepsMoveP6++;
+	        }
+	           
+	        //collision checking
+	        //bounds.setPosition(position.x, position.y);
+	        //if (map.isColliding(bounds)) {
+	        //	System.out.println("Minion collided! Resetting position.");
+	        //    position.x = permittedX;
+	        //    position.y = permittedY;
+	        //}
+	        
+	        //ScreenBounds(); //character wont walk out of frame
 	       
 	    }
-      
+	      
 	    private void ScreenBounds() {
 	        if (position.x < 0) position.x = 0;
 	        if (position.x > MAP_WIDTH * DEFAULT_WIDTH - DEFAULT_WIDTH)
