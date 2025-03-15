0e61c98e9b78b67ebcdadf725f7435956e1e7376
Song Jie Min Jaymee <2402821@sit.singaporetech.edu.sg>
2025-03-15 17:00:17 +0800

    Change of food shapes, randomizing of food spawn time, randomized spawned food

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Minion.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Minion.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import java.util.ArrayList;
 
-
 public class Minion extends AbstractMovableObject implements Targetable{
 
     private static final int DEFAULT_WIDTH = 32;  //the minion width
